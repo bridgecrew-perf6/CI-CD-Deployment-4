5e5cf0a2d38885827e02ce24a5d3017f2c06ef1c
jyothi <kothapallyjyothi95@example.com>
2022-01-23 21:51:44 +0530

    first commit

---
+++ src/test/java/com/example/JenkinsDeploy/JenkinsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
